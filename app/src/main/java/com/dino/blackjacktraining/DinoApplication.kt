74d952eeb7bc24d2bd0074d6d664fa6fb2a7dc37
sjjeong <sjjeong1225@gmail.com>
2019-11-23 20:32:08 +0900

    메인에 하단 배너 추가

--- app/src/main/java/com/dino/blackjacktraining/DinoApplication.kt
+++ app/src/main/java/com/dino/blackjacktraining/DinoApplication.kt
@@ -3,11 +3,13 @@ package com.dino.blackjacktraining
 import android.app.Application
 import com.dino.blackjacktraining.di.vieWModelModule
 import com.dino.library.ext.setupKoin
+import com.google.android.gms.ads.MobileAds
 
 class DinoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        MobileAds.initialize(this)
         setupKoin(this, vieWModelModule)
     }
 }
--- app/src/main/java/com/dino/blackjacktraining/ui/MainActivity.kt
+++ app/src/main/java/com/dino/blackjacktraining/ui/MainActivity.kt
@@ -1,10 +1,23 @@
 package com.dino.blackjacktraining.ui
 
+import android.os.Bundle
+import com.dino.blackjacktraining.BuildConfig
 import com.dino.blackjacktraining.R
 import com.dino.blackjacktraining.databinding.ActivityMainBinding
 import com.dino.library.ui.DinoActivity
+import com.google.android.gms.ads.AdRequest
 
 class MainActivity : DinoActivity<ActivityMainBinding, MainViewModel>(
     R.layout.activity_main,
     MainViewModel::class.java
-)
+) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding {
+            if (!BuildConfig.DEBUG) {
+                adView.loadAd(AdRequest.Builder().build())
+            }
+        }
+    }
+}
